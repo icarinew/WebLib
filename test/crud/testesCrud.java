6e5c68618507a72ba2fee31afc7cc94d7e4a9320
AsaphSouza <asaphsouza1605@gmail.com>
2018-11-27 19:00:54 -0200

    insercao dos testes de buscas

---
+++ test/crud/testesCrud.java
@@ -0,0 +1,61 @@
+package crud;
+
+import controllerWebLib.LivroController;
+import controllerWebLib.TextoController;
+import controllerWebLib.UsuarioController;
+import modelWebLib.Entidades.Livro;
+import modelWebLib.Entidades.Texto;
+import modelWebLib.Entidades.Usuario;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/*
+ * @author asaph
+ */
+public class testesCrud {
+    
+    public testesCrud() {
+    }
+    
+    @Test
+    public void testeBuscarUsuario (Usuario user) {
+        UsuarioController uc = new UsuarioController();
+        uc.setU(user);
+        assertEquals(user.getId_usuario(), uc.BuscarUsuario().getId_usuario());
+    }
+    
+    @Test
+    public void testeBuscarLivro (Livro book) {
+        LivroController lc = new LivroController();
+        lc.setLivro(book);
+        assertEquals(book.getId(), lc.BuscarLivro().getId());
+    }
+    
+    @Test 
+    public void testeBuscarTexto (Texto text) {
+        TextoController tc = new TextoController();
+        tc.setTexto(text);
+        assertEquals(text.getId(), tc.BuscarTexto().getId());
+    }
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+}
